a10472ce7226799471e55711ebadd0fad81bb277
Sourabh <sourabh@Sourabhs-MacBook-Air.local>
2023-07-09 19:14:14 +0530

    Initial setup

---
+++ src/main/java/com/scoreit/ScoreItServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scoreit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreItServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreItServiceApplication.class, args);
+	}
+
+}
