ab17ae112060989b7fe55e2baad7f25a441b635a
Sourabh Berde <sourabh.berde44@gmail.com>
2023-08-23 13:31:05 +0530

    Configuration step

--- src/main/java/com/scoreit/controller/PlayerController.java
+++ src/main/java/com/scoreit/controller/PlayerController.java
@@ -1,27 +1,21 @@
 package com.scoreit.controller;
 
-import com.scoreit.model.entity.Player;
 import com.scoreit.model.request.PlayerDto;
-import com.scoreit.service.PlayerServiceImpl;
 import java.util.logging.Logger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/players")
 @RequiredArgsConstructor
 public class PlayerController {
     private static final Logger LOGGER = Logger.getLogger(PlayerController.class.getName());
 
-    private final PlayerServiceImpl playerService;
-
-    @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Player create(@RequestBody PlayerDto player) throws Exception {
-        LOGGER.info("Received player creation request");
-        return playerService.create(player);
+    @PostMapping(path = "/")
+    public String create(@RequestBody PlayerDto player) throws Exception {
+        return "Player Creation request received";
     }
 }
--- src/main/java/com/scoreit/service/PlayerService.java
+++
@@ -1,9 +0,0 @@
-package com.scoreit.service;
-
-import com.scoreit.model.entity.Player;
-import com.scoreit.model.request.PlayerDto;
-
-public interface PlayerService {
-
-    Player create(PlayerDto player) throws Exception;
-}
